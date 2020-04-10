ee643e44a3fdcedbb675bbab0fd2621769b79e08
Thanh Vu <thanh.vu@savvycomsoftware.com>
2020-04-11 03:25:23 +0700

    correct make k8s spec script

--- scripts/make-kubenetes-spec.sh
+++ scripts/make-kubenetes-spec.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for t in $(find ./deploy/k8s -type f -name "*.yaml"); do \
+for t in $(find ./deployments/kubernetes -type f -name "*.yaml"); do \
     envsubst < $t; \
     echo '---'; \
 done > k8s-spec.yaml
